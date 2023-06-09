bae7e9e11f1f0471a36bd261699d750ed2eff2e2
sohan <gamalathsohan@gmail.com>
2023-06-09 20:27:08 +0530

    tariif data display implemented

--- src/pages/UserDashboard/components/TarifficDataTable.js
+++ src/pages/UserDashboard/components/TarifficDataTable.js
@@ -1,36 +1,68 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 import React from "react";
+import { FiDownload } from "react-icons/fi";
 
 function TarifficDataTable() {
+  const pdfDownload = () => {
+    const fileUrl = process.env.PUBLIC_URL + "/Tariff.pdf";
+    console.log(fileUrl);
+    const link = document.createElement("a");
+    link.href = fileUrl;
+    link.setAttribute("download", "results.pdf");
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
   return (
     <>
       <Container
         sx={{
           mt: 15,
           ml: 38,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
+          pb: 5,
+          width: "100%",
         }}
       >
-        <Typography sx={{ fontSize: 90 }}>COMING SOON</Typography>
-        <Typography sx={{ fontSize: 25 }}>
-          This feature is under development.
-        </Typography>
-        <Typography sx={{ fontSize: 20 }}>
-          Stay tune for something amazing.
-        </Typography>
-
         <Box
-          component="img"
           sx={{
-            height: 350,
-            width: 650,
-            mt: 3,
+            width: "70vw",
+            alignItems: "center",
+            display: "flex",
+            justifyContent: "space-evenly",
           }}
-          alt="The house from the offer."
-          src="https://t4.ftcdn.net/jpg/03/85/25/03/360_F_385250385_zywKHrbWbUnqU3Fap1PsEsRvKuAC67lw.jpg"
-        />
+        >
+          <img src="/tariffData.jpg" width={"100%"} />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            mt: 5,
+            mb: 5,
+            alignItems: "center",
+            width: "70vw",
+          }}
+        >
+          <Typography
+            sx={{
+              mr: 0.5,
+              backgroundColor: "#1e1e1f",
+              color: "white",
+              py: 1,
+              px: 5,
+            }}
+          >
+            Download Tariff Data
+          </Typography>
+          <Button
+            variant="contained"
+            color="info"
+            startIcon={<FiDownload />}
+            onClick={pdfDownload}
+          >
+            Download
+          </Button>
+        </Box>
       </Container>
     </>
   );

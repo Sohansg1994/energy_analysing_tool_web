1209988b0ef70442e62263242e4d6446c65a9578
sohan-teaminvos <sohan@invos.global>
2023-11-18 19:06:05 +0530

    feat:Payement Details updated

--- src/pages/PaymentDetails.js
+++ src/pages/PaymentDetails.js
@@ -1,5 +1,55 @@
+import { Box, Container, Typography } from "@mui/material";
 import React from "react";
 
 export default function PaymentDetails() {
-  return <div>Payment Details</div>;
+  return (
+    <>
+      <Container
+        sx={{
+          mt: 20,
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <h2>Payment Method</h2>
+        <Box
+          sx={{
+            alignItems: "left",
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: 18,
+            }}
+          >
+            Send an SMS or WhatsApp to:Geenbill service hotline - 07522385095
+          </Typography>
+          <Typography
+            sx={{
+              fontSize: 18,
+            }}
+          >
+            The instructions will be sent.
+          </Typography>
+          <Typography
+            sx={{
+              fontSize: 18,
+            }}
+          >
+            Domestic Lite charge - Rs.3000/-
+          </Typography>
+          <Typography
+            sx={{
+              mt: 5,
+            }}
+          >
+            Thank You
+          </Typography>
+          <Typography>Greenbill Service</Typography>
+        </Box>
+      </Container>
+    </>
+  );
 }
